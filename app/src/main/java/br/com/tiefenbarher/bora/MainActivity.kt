c1d18ab5be8dbd2789465a505202072b4de2ba13
tiefz <chinorex@gmail.com>
2023-11-07 22:08:32 -0300

    Convertemos o laytou para bindingview

--- app/src/main/java/br/com/tiefenbarher/bora/MainActivity.kt
+++ app/src/main/java/br/com/tiefenbarher/bora/MainActivity.kt
@@ -1,12 +1,26 @@
 package br.com.tiefenbarher.bora
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import br.com.tiefenbarher.bora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.apply {
+
+            btBotao1.setOnClickListener {
+                tvHello.text = "teste"
+            }
+            btBotao2.setOnClickListener {
+                tvHello.text = "teste do botao 2"
+            }
+
+        }
     }
 }
